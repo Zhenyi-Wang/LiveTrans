1bb1cec572299f085590ad78f11c82c4f0cf51f9
Zhenyi Wang <zhenyiwang@intl.zju.edu.cn>
2025-10-02 14:35:54 +0800

    feat: 更新模型路径并修改直播流地址

--- whisperlive/run_client.py
+++ whisperlive/run_client.py
@@ -6,7 +6,9 @@ client = TranscriptionClient(
     "localhost",
     9090,
     # model="/home/zhenyi/ownprojects/livetrans/models/faster-whisper-large-v2",
-    model="large-v2",
+    # model="/home/zhenyi/models/whispers/whisper-large-v2-finetune-timestamps-ct2",
+    model="/home/zhenyi/models/whispers/whisper-large-v2-finetune-no-timestamps-ct2-new",
+    # model="large-v2",
     # model="/home/zhenyi/ownprojects/livetrans/models/faster-whisper-large-v3-turbo-ct2",
     # model="/home/zhenyi/models/transCT2/Belle-whisper-large-v3-turbo-zh-ct2",
     # model="/home/zhenyi/models/transCT2/Belle-distilwhisper-large-v2-zh-ct2",
@@ -19,4 +21,5 @@ client = TranscriptionClient(
     dispatch_api="http://mini:8081/backend/api/listen",
 )
 # client(other_url="https://realtime.hainingchurch.cn/live/livestream.m3u8")
-client(other_url="https://realtime.hainingchurch.cn/live/livestream.flv")
+# client(other_url="https://realtime.hainingchurch.cn/live/livestream.flv")
+client(other_url="http://mini:8080/live/livestream.flv")
